f2540919dda921d0ed983165103ed1ab8a8a579c
Anindita Som <aninditasom2020@gmail.com>
2021-01-23 22:38:13 +0000

    add task form

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@
     $('.sidenav').sidenav({edge: "right"})
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        showClearBtn: true,
+        yearRange: 3,
+        i18n:{
+            done: "Select"
+        }
+    });
 });
