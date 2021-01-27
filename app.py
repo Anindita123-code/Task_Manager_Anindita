1ddcc5586157a24e4c147bc850cdd6f4176d4967
Anindita Som <aninditasom2020@gmail.com>
2021-01-27 23:41:24 +0000

    Edit category

--- app.py
+++ app.py
@@ -148,12 +148,26 @@ def show_categories():
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
-        category = { "category_name": request.form.get("category_name") }
+        category = {"category_name": request.form.get("category_name")}
         mongo.db.categories.insert_one(category)
         flash("New Category Added")
         return redirect(url_for('show_categories'))
 
     return render_template("add_category.html")
+
+
+@app.route('/edit_category/<category_id>', methods=["GET", "POST"])
+def edit_category(category_id):
+    if request.method == "POST":
+        submit = {
+                    "category_name": request.form.get("category_name")
+                }
+        mongo.db.categories.update({"_id": ObjectId(category_id)}, submit)
+        flash("Category Updated Successfully!")
+        return redirect(url_for('show_categories'))
+
+    category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
+    return render_template("edit_category.html", category=category)
 
 
 if __name__ == "__main__":
